23abcd95371c0c98dae6edf41e0f101d850a5141
gengkang <gengkang@xnw.com>
2017-06-10 14:34:35 +0800

    improve br switch

--- resources/scripts/xnw/Release.sh
+++ resources/scripts/xnw/Release.sh
@@ -15,25 +15,42 @@ function ce() {
    fi
 }
 
+function del_old_br() {
+   br_list=(`git branch`)
+   for br in "${br_list[@]}";
+   do
+        if [[ "${br}" == "{{ branch }}" ]]; then
+            echo -e "删除旧分支...\n"
+            git branch -D "${br}"
+            ce  "删除旧分支"
+        fi
+   done
+}
+
 if [[ "${now_br}" == "{{ branch }}" ]];then
     echo -e "生成临时分支...\n"
     git checkout -b ${tmp_br}
     ce "生成临时分支"
 
-    echo -e "删除旧分支...\n"
-    git branch -D ${now_br}
-    ce  "删除旧分支"
+    del_old_br
 
     echo -e "co 新分支...\n"
-    git chekcout "{{ branch }}"
+    git checkout "{{ branch }}"
     ce "co 新分支"
 else
+    del_old_br
+
     echo -e "co 新分支...\n"
     git checkout "{{ branch }}"
      ce "co 新分支"
 fi
 
+#
+# grunt release && sassc && artc
+# cd laravel
+# composer install -o --no-dev
+# yarn
+# npm run production
+# php artisan up
+#
 echo -e "{{ server_name }} 上线完成....\n"
-
-
-
